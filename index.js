5cf534c55a5ab96f925dcd485b0350a420582a76
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-07 16:16:37 -0600

    added employeeQuestions logic to create constructors based on position chosen

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 
 const questions = ["What is the team manager's name?", "What is his/her employee ID?", "What is his/her email address?", "Please select an employee to add:"]
-
+const newEmployeeQuestions = ["What is his/her name?", "What is his/her employee ID?", "What is his/her email address?"]
 
 function init(){
 inquirer
@@ -49,12 +49,42 @@ function newEmployeeAdd(){
         .then((data) => {
             if (data.newEmployee === "Engineer"){
                 console.log("Add Engineer");
-                newEmployeeAdd();
+                employeeQuestions(data.newEmployee);
             } else if (data.newEmployee === "Intern"){
                 console.log("Add Intern");
-                newEmployeeAdd();
+                employeeQuestions(data.newEmployee);
             } else {
                 return "Team finished";
+            }
+        })
+}
+
+function employeeQuestions(position){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: newEmployeeQuestions[0],
+                name: 'enteredName',
+            },
+            {
+                type: 'input',
+                message: newEmployeeQuestions[1],
+                name: 'ID',
+            },
+            {
+                type: 'input',
+                message: newEmployeeQuestions[2],
+                name: 'emailAddress',
+            },
+        ])
+        .then((data) => {
+            if(position === "Engineer"){
+                const newEngineer = new Engineer(data.enteredName, data.ID, data.emailAddress);
+                newEmployeeAdd();
+            } else if (position === "Intern"){
+                console.log("create new intern");
+                newEmployeeAdd();
             }
         })
 }
