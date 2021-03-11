510e4614a0493382d55e47969bff23b23890ff90
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-11 09:16:51 -0600

    logic for creating HTML based on number of team members

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -53,12 +53,105 @@ function createHTML(teamMembers){
     cards.push(card[i]);
     }
 console.log("cards is " + cards);
+generateHTML(cards);
 }
 
 
 // function to generate markdown for README
-function generateHTML(teamMembers) {
-    return `HTML GOES HERE`;
+function generateHTML(cards) {
+    if (cards.length <=3){
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Team</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+            <link href="style.css" rel="stylesheet">
+        </head>
+        <body>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">My Team</span>
+                </div>
+            </nav>
+        <div class="row">
+            ${cards[0]}
+            ${cards[1]}
+            ${cards[2]}
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        </body>
+        </html>`
+    }
+    else if (cards.length <= 6){
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Team</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+            <link href="style.css" rel="stylesheet">
+        </head>
+        <body>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">My Team</span>
+                </div>
+            </nav>
+        <div class="row">
+            ${cards[0]}
+            ${cards[1]}
+            ${cards[2]}
+        </div>
+        <div class="row">
+            ${cards[3]}
+            ${cards[4]}
+            ${cards[5]}
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        </body>
+        </html>`
+    } 
+    else {
+        return `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>My Team</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+            <link href="style.css" rel="stylesheet">
+        </head>
+        <body>
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">My Team</span>
+                </div>
+            </nav>
+        <div class="row">
+            ${cards[0]}
+            ${cards[1]}
+            ${cards[2]}
+        </div>
+        <div class="row">
+            ${cards[3]}
+            ${cards[4]}
+            ${cards[5]}
+        </div>
+        <div class="row">
+            ${cards[6]}
+            ${cards[7]}
+            ${cards[8]}
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+        </body>
+        </html>`
+    }
   }
   
   module.exports = {createHTML, generateHTML};
