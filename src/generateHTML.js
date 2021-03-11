5a27760810ce164aef73dccfb75a321315681747
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-11 08:58:57 -0600

    logic for card creation based on role

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -4,19 +4,52 @@ let card=[];
 // create number of cards template per the number of team members.
 function createHTML(teamMembers){
     for (let i = 0; i < teamMembers.length; i++){
-        card[i] = `<div class="col">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <h5 class="card-title">${teamMembers[i].getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${teamMembers[i].getRole()}</h6>
+        if(teamMembers[i].getRole() === "Manager"){
+            card[i] =
+        `<div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${teamMembers[i].getName()}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${teamMembers[i].getRole()}</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
+                    <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>      
+                    <li class="list-group-item">Room Number: ${teamMembers[i].roomNumber}</li>
+                </ul>
             </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
-                <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
-        </div>
-    </div>`
+        </div>`} 
+        else if(teamMembers[i].getRole() === "Engineer"){
+            card[i] = 
+        `<div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${teamMembers[i].getName()}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${teamMembers[i].getRole()}</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
+                    <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>
+                    <li class="list-group-item">GitHub User Name: ${teamMembers[i].gitHubUserName}</li>
+                </ul>
+            </div>
+        </div>`
+    } else {
+        card[i] = 
+        `<div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">${teamMembers[i].getName()}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${teamMembers[i].getRole()}</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
+                    <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>
+                    <li class="list-group-item">School: ${teamMembers[i].school}</li>
+                </ul>
+            </div>
+        </div>`
+    }
     cards.push(card[i]);
     }
 console.log("cards is " + cards);
