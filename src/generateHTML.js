efa7c4dc485297a645036542a02728df67cd16f4
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-03-11 10:23:40 -0600

    added links to email and github

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -14,7 +14,7 @@ function createHTML(teamMembers){
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
-                    <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>      
+                    <li class="list-group-item">Email: <a href="mailto:${teamMembers[i].getEmail()}">${teamMembers[i].getEmail()}</a></li>      
                     <li class="list-group-item">Room Number: ${teamMembers[i].roomNumber}</li>
                 </ul>
             </div>
@@ -29,8 +29,8 @@ function createHTML(teamMembers){
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
-                    <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>
-                    <li class="list-group-item">GitHub User Name: ${teamMembers[i].gitHubUserName}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${teamMembers[i].getEmail()}">${teamMembers[i].getEmail()}</a></li>      
+                    <li class="list-group-item">GitHub User Name: <a href="https://github.com/${teamMembers[i].gitHubUserName}" target="_blank">${teamMembers[i].gitHubUserName}</a></li>
                 </ul>
             </div>
         </div>`
@@ -44,7 +44,7 @@ function createHTML(teamMembers){
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">ID: ${teamMembers[i].getID()}</li>
-                    <li class="list-group-item">Email: ${teamMembers[i].getEmail()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${teamMembers[i].getEmail()}">${teamMembers[i].getEmail()}</a></li>      
                     <li class="list-group-item">School: ${teamMembers[i].school}</li>
                 </ul>
             </div>
@@ -60,99 +60,99 @@ console.log("cards is " + cards);
 function generateHTML() {
     if (cards.length <=3){
         return `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My Team</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-            <link href="style.css" rel="stylesheet">
-        </head>
-        <body>
-            <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">My Team</span>
-                </div>
-            </nav>
-        <div class="row">
-            ${cards[0]}
-            ${cards[1]}
-            ${cards[2]}
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-        </body>
-        </html>`
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">My Team</span>
+            </div>
+        </nav>
+    <div class="row">
+        ${cards[0]}
+        ${cards[1]}
+        ${cards[2]}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+    </html>`
     }
     else if (cards.length <= 6){
         return `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My Team</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-            <link href="style.css" rel="stylesheet">
-        </head>
-        <body>
-            <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">My Team</span>
-                </div>
-            </nav>
-        <div class="row">
-            ${cards[0]}
-            ${cards[1]}
-            ${cards[2]}
-        </div>
-        <br>
-        <div class="row">
-            ${cards[3]}
-            ${cards[4]}
-            ${cards[5]}
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-        </body>
-        </html>`
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">My Team</span>
+            </div>
+        </nav>
+    <div class="row">
+        ${cards[0]}
+        ${cards[1]}
+        ${cards[2]}
+    </div>
+    <br>
+    <div class="row">
+        ${cards[3]}
+        ${cards[4]}
+        ${cards[5]}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+    </html>`
     } 
     else {
         return `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My Team</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-            <link href="style.css" rel="stylesheet">
-        </head>
-        <body>
-            <nav class="navbar navbar-light bg-light">
-                <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">My Team</span>
-                </div>
-            </nav>
-        <div class="row">
-            ${cards[0]}
-            ${cards[1]}
-            ${cards[2]}
-        </div>
-        <br>
-        <div class="row">
-            ${cards[3]}
-            ${cards[4]}
-            ${cards[5]}
-        </div>
-        <br>
-        <div class="row">
-            ${cards[6]}
-            ${cards[7]}
-            ${cards[8]}
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-        </body>
-        </html>`
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">My Team</span>
+            </div>
+        </nav>
+    <div class="row">
+        ${cards[0]}
+        ${cards[1]}
+        ${cards[2]}
+    </div>
+    <br>
+    <div class="row">
+        ${cards[3]}
+        ${cards[4]}
+        ${cards[5]}
+    </div>
+    <br>
+    <div class="row">
+        ${cards[6]}
+        ${cards[7]}
+        ${cards[8]}
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    </body>
+    </html>`
     }
   }
   
